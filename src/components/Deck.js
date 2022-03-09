26644d1fe7935734730206e67dffdf58ddd13bed
André Moreira <andre.moreira@codeminer42.com>
2022-03-09 16:31:37 -0300

    Add reviewer fixes

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,7 +4,8 @@ const _ = require('lodash');
 
 const SUITS = ["spades", "hearts", "clubs", "diamonds"];
 const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+const LINE_SIZE = 7
+const LINE_SHIFT = 2
 export const generateDeck = () => { 
   const initialDeck = SUITS.flatMap((suit) => VALUES.flatMap(value => ({suit: suit, value: value})));
   return _.shuffle(initialDeck).slice(0, 21);
@@ -14,10 +15,11 @@ export const generateDeck = () => {
 function Deck() {
   const [deck, setDeck]  = useState(generateDeck());
 
-  const trickStep = (initialLine) => { 
+  const trickStep = (Line) => { 
+    
     //Place chosen line on the middle of the array
-    const initialPosition = (initialLine)*7; //0, 7 or 14 new array start
-    const stackedDeck = [[...deck].splice(initialPosition, 21), [...deck].splice(0, initialPosition)].flat()
+    const initialPosition = (Line+LINE_SHIFT)*LINE_SIZE%deck.length; //0, 7 or 14 new array start
+    const stackedDeck = [[...deck].splice(initialPosition, deck.length), [...deck].splice(0, initialPosition)].flat()
     
     //Reorder cards in three lines
     const newDeck = stackedDeck.reduce((acc, element, index) =>{
@@ -31,9 +33,9 @@ function Deck() {
   return (
   <div id='main' >
     <div id='buttons'>
-      <button onClick={() => trickStep(2)}>Linha 1</button>
-      <button onClick={() => trickStep(0)}>Linha 2</button>
-      <button onClick={() => trickStep(1)}>Linha 3</button>
+      <button onClick={() => trickStep(0)}>Linha 1</button>
+      <button onClick={() => trickStep(1)}>Linha 2</button>
+      <button onClick={() => trickStep(2)}>Linha 3</button>
     </div>
     <div role='deck' id='cards'>
       {deck.map(({ value, suit }, index) =>
